c4cc6d113c64c4d3e8711518b307e0b479012d6e
Muhammad Sohan Mollah <sohansourab@gmail.com>
2025-01-08 00:27:33 +0600

    responsive mobile

--- components/common/CourseCard.jsx
+++ components/common/CourseCard.jsx
@@ -1,36 +1,35 @@
 import React from 'react'
 import Image from 'next/image';
 
-import course1 from "@/public/home/courses/course1.jpeg";
-import lesson from "@/public/home/courses/lesson.svg";
+import lessonImg from "@/public/home/courses/lesson.svg";
 import clock from "@/public/home/courses/clock.svg";
 import Link from 'next/link';
 
-const CourseCard = ({ varient }) => {
+const CourseCard = ({ varient, image, alt, category, title, lesson, time, link }) => {
     return (
-        <Link href="/course">
+        <Link href={link}>
             <div className='rounded-lg overflow-hidden flex-col bg-white shadow-sm'>
                 <div className='relative'>
-                    <Image src={course1} alt='course Image' className='object-cover' />
+                    <Image src={image} alt={alt} className='object-cover' />
                     {
                         varient == "home" ? '' :
-                            <span className='bg-[#EFF1FD] absolute top-5 left-5 px-2 py-1 text-primary capitalize rounded-lg'>intermediate</span>
+                            <span className='bg-[#EFF1FD] absolute top-5 left-5 px-2 py-1 text-primary capitalize rounded-lg'>{category}</span>
                     }
                 </div>
                 <div className='p-5 space-y-2'>
                     {
                         varient == "home" &&
-                        <h4 className='text-black-900 text-sm capitalize font-medium'>Cloud computing</h4>
+                        <h4 className='text-black-900 text-sm capitalize font-medium'>{category}</h4>
                     }
-                    <h2 className='capitalize font-semibold text-base text-black'>Master the Core Protocol of the Internet of Things...</h2>
+                    <h2 className='capitalize font-semibold text-base text-black'>{title}</h2>
                     <div className='flex justify-between items-center'>
                         <div className='flex gap-3 items-center justify-center text-black-900'>
-                            <Image src={lesson} alt='lesson icon' className='object-cover' />
-                            32 Lessons
+                            <Image src={lessonImg} alt='lesson icon' className='object-cover' />
+                            {lesson} Lessons
                         </div>
                         <div className='flex gap-3 items-center justify-center text-black-900'>
                             <Image src={clock} alt='clock icon' className='object-cover' />
-                            3.4 Hours
+                            {time} Hours
                         </div>
                     </div>
                 </div>
--- components/home/Courses.jsx
+++ components/home/Courses.jsx
@@ -3,17 +3,55 @@ import Heading from '../common/Heading'
 import SubHeading from '../common/SubHeading'
 import CourseCard from '../common/CourseCard'
 
+import course1 from "@/public/home/courses/course1.jpeg";
+
 const Courses = () => {
     return (
-        <div className='bg-tertiary py-24'>
+        <div className='bg-tertiary px-5 py-10 md:py-24'>
             <div className='container space-y-6'>
                 <Heading text="Our Courses" />
-                <SubHeading cn="px-56" text="Learn to design and program embedded systems with practical, real-world applications. Build expertise in hardware-software integration for cutting-edge technology!" />
+                <SubHeading cn="px-0 md:px-56" text="Learn to design and program embedded systems with practical, real-world applications. Build expertise in hardware-software integration for cutting-edge technology!" />
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10'>
-                    <CourseCard varient='home' />
-                    <CourseCard varient='home' />
-                    <CourseCard varient='home' />
-                    <CourseCard varient='home' />
+                    <CourseCard
+                        varient='home'
+                        image={course1}
+                        alt='course1_image'
+                        category="Imtermediate"
+                        title="Bluetooth Low Energy (BLE) From Ground Up"
+                        lesson='32'
+                        time='10'
+                        link='/'
+                    />
+                    <CourseCard
+                        varient=''
+                        image={course1}
+                        alt='course1_image'
+                        category="Imtermediate"
+                        title="Bluetooth Low Energy (BLE) From Ground Up"
+                        lesson='32'
+                        time='10'
+                        link='/'
+                    />
+                    <CourseCard
+                        varient='home'
+                        image={course1}
+                        alt='course1_image'
+                        category="Imtermediate"
+                        title="Bluetooth Low Energy (BLE) From Ground Up"
+                        lesson='32'
+                        time='10'
+                        link='/'
+                    />
+                    <CourseCard
+                        varient='home'
+                        image={course1}
+                        alt='course1_image'
+                        category="Imtermediate"
+                        title="Bluetooth Low Energy (BLE) From Ground Up"
+                        lesson='32'
+                        time='10'
+                        link='/'
+                    />
                 </div>
             </div>
         </div>
