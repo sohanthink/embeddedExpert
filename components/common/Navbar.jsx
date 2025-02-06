da7f30824d24c11960d6992c08a1588ebf8fc9b8
Muhammad Sohan Mollah <sohansourab@gmail.com>
2025-02-06 23:18:00 +0600

    nav color change

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
         setIsMenuOpen(false);
     }
     return (
-        <nav className='container md:w-[70%] absolute md:fixed md:bg-tertiary/80 rounded-full top-0 md:top-5 left-1/2 transform -translate-x-1/2 flex justify-between items-center px-5 md:px-10 py-4 z-[999]'>
+        <nav className='container md:w-[70%] absolute md:fixed md:bg-[#10192cf3] rounded-full top-0 md:top-5 left-1/2 transform -translate-x-1/2 flex justify-between items-center px-5 md:px-10 py-4 z-[999]'>
             <div className='w-32 md:w-44'><Link href='/'><Image src={logo} alt='logo' /></Link></div>
             <div className='flex gap-3 md:gap-8 items-center'>
                 {
@@ -45,19 +45,19 @@ const Navbar = () => {
                 }
                 <ul className={`text-black gap-8 text-sm ${isMenuOpen ? 'flex h-[85vh] items-center justify-center absolute top-0' : 'hidden'} md:flex flex-col md:flex-row md:static top-16 left-0 w-full md:w-auto bg-white md:bg-transparent p-5 md:p-0 z-10`}>
                     <li>
-                        <Link href='/' className={pathname === '/' ? 'text-blue-500 font-bold' : 'text-black'} onClick={closeMenu}>Home</Link>
+                        <Link href='/' className={pathname === '/' ? 'text-blue-500 font-bold' : 'text-black md:text-white'} onClick={closeMenu}>Home</Link>
                     </li>
                     <li>
-                        <Link href='/#contact' className={pathname === '/#contact' ? 'text-blue-500 font-bold' : 'text-black'} onClick={closeMenu}>Get In Touch</Link>
+                        <Link href='/#contact' className={pathname === '/#contact' ? 'text-blue-500 font-bold' : 'text-black md:text-white'} onClick={closeMenu}>Get In Touch</Link>
                     </li>
                     <li>
-                        <Link href='/affiliate' className={pathname === '/affiliate' ? 'text-blue-500 font-bold' : 'text-black'} onClick={closeMenu}>Affiliate</Link>
+                        <Link href='/affiliate' className={pathname === '/affiliate' ? 'text-blue-500 font-bold' : 'text-black md:text-white'} onClick={closeMenu}>Affiliate</Link>
                     </li>
                     <li>
-                        <Link href='/resources' className={pathname === '/resources' ? 'text-blue-500 font-bold' : 'text-black'} onClick={closeMenu}>Resources</Link>
+                        <Link href='/resources' className={pathname === '/resources' ? 'text-blue-500 font-bold' : 'text-black md:text-white'} onClick={closeMenu}>Resources</Link>
                     </li>
                     <li>
-                        <Link href='/courses' className={pathname === '/courses' ? 'text-blue-500 font-bold' : 'text-black'} onClick={closeMenu}>Join A Course</Link>
+                        <Link href='/courses' className={pathname === '/courses' ? 'text-blue-500 font-bold' : 'text-black md:text-white'} onClick={closeMenu}>Join A Course</Link>
                     </li>
                 </ul>
                 {
